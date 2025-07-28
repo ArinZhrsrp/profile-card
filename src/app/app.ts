9ba5d9097ec607fdd3c84222b1f3286499ae73a4
Shirin Zahra <shiwwinzahra@gmail.com>
2025-07-28 12:00:15 +0800

    Add two way binding for changing user details

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'profile-card';
+
+  name: string = 'John Doe';
+  age: number = 30;
+  description: string = 'A passionate developer learning Angular';
 }
